3b762117b825a49234f35a627f2266f384908fd6
Ronney <Ronney@Loomer.lan>
2017-06-12 10:31:31 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+const nineBtn = document.querySelector("#nine");
+const divideBtn = document.querySelector("#divide");
+const fourBtn = document.querySelector("#four");
+const fiveBtn = document.querySelector("#five");
+const sixBtn = document.querySelector("#six");
+const multiplyBtn = document.querySelector("#multiply");
+const oneBtn = document.querySelector("#one");
+const twoBtn = document.querySelector("#two");
+const threeBtn = document.querySelector("#three");
+const minusBtn = document.querySelector("#minus");
+const zeroBtn = document.querySelector("#zero");
+const dotBtn = document.querySelector("#dot");
+const equalsBtn = document.querySelector("#equals");
+const plusBtn = document.querySelector("#plus");
+
+let btnList = document.querySelectorAll(".squares");
+
+
+for (let i = 0; i < btnList.length; i++) {
+  let btn = btnList[i];
+
+  btn.addEventListener("click", function(event) {
+
+  });
+}
